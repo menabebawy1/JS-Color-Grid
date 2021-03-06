35e50dbd7020a8179a784df826461310dc7eccbb
caesar-chin <56019561+caesar-chin@users.noreply.github.com>
2021-03-06 16:50:18 -0500

    Fixed functions

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let numRows = 0;
 let numCols = 0;
 let colorSelected;
-let table = document.getElementById('grid');
+let table = document.getElementById("grid");
 
 function addR() {
   //add a TR
   let col;
-  let row = document.createElement('TR');
+  let row = document.createElement("TR");
   table.appendChild(row);
 
   //Inside the TR, add the number of columns as TD's
@@ -16,7 +16,7 @@ function addR() {
   }
 
   for (let i = 0; i < numCols; i++) {
-    col = document.createElement('TD');
+    col = document.createElement("TD");
     row.appendChild(col);
   }
 
@@ -31,13 +31,13 @@ function addC() {
   if (numRows === 0) {
     //When we don't have any rows yet, just adding 1 square/column will increase the number of rows
     numRows++;
-    let extra = document.createElement('TR');
+    let extra = document.createElement("TR");
     table.appendChild(extra);
   }
 
   for (let i = 0; i < numRows; i++) {
-    row = document.querySelectorAll('tr')[i];
-    col = document.createElement('TD');
+    row = document.querySelectorAll("tr")[i];
+    col = document.createElement("TD");
     row.appendChild(col);
   }
 
@@ -45,42 +45,39 @@ function addC() {
 }
 
 function removeR() {
-  let row = table.rows.length - 1; //gets the index of the last row
-
-  //edge case if there are none to remove
-  if (numCols <= 0 || numRows <= 0) {
-    alert('You need to add a row or column!');
+  if (numRows === 0) {
+    alert("You need to add a row!");
   } else {
-    document.getElementById('grid').deleteRow(row);
-    numRows -= 1;
+    table.deleteRow(numRows - 1);
+    numRows--;
+    // console.log(`rows: ${numRows}`);
   }
 }
 
 function removeC() {
-  let col = document.getElementById('grid').rows[0].cells.length - 1;
-
-  //edge case if there are none to remove
-  if (numCols <= 0 || numRows <= 0) {
-    alert('You need to add a row or column!');
+  if (numCols === 0) {
+    alert("You need to add a column!");
   } else {
-    for (let i = 0; i < grid.rows.length; i++) {
-      grid.rows[i].deleteCell(col);
-      numRows -= 1;
+    let row = document.querySelectorAll("tr");
+    for (let i = 0; i < numRows; i++) {
+      row[i].removeChild(row[i].lastChild);
     }
+    numCols--;
+    // console.log(`col: ${numCols}`);
   }
 }
 
 // selected() begin
 function selected() {
-  let colors = ['Red', 'Green', 'Blue', 'Yellow'];
-  let selected = document.getElementById('selectedID').value;
+  let colors = ["Red", "Green", "Blue", "Yellow"];
+  let selected = document.getElementById("selectedID").value;
   if (colors.find((element) => element === selected) !== undefined) {
     colorSelected = selected;
   }
-  table.addEventListener('click', (event) => {
-    if (event.target.matches('td')) {
+  table.addEventListener("click", (event) => {
+    if (event.target.matches("td")) {
       event.target.style.backgroundColor = document.getElementById(
-        'selectedID'
+        "selectedID"
       ).value;
     }
   });
@@ -88,18 +85,18 @@ function selected() {
 // selected() end
 
 function fill() {
-  alert('Clicked Fill All');
+  alert("Clicked Fill All");
 }
 
 function fillU() {
-  alert('Clicked Fill All Uncolored');
+  alert("Clicked Fill All Uncolored");
 }
 
 //clearAll() begin
 function clearAll() {
   for (let i = 0; i < table.childNodes.length; i++) {
     for (let j = 0; j < table.childNodes[i].childNodes.length; j++) {
-      table.childNodes[i].childNodes[j].style.backgroundColor = 'white';
+      table.childNodes[i].childNodes[j].style.backgroundColor = "white";
     }
   }
 }
